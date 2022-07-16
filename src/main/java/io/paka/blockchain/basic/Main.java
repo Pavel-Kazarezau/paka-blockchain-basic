dd69ec9afb76a50ffc67b3ea180b75c6f6b58704
paka <p.kazarezov@gmail.com>
2022-07-16 18:13:40 +0300

    Refactor program starter

--- src/main/java/io/paka/blockchain/basic/Main.java
+++ src/main/java/io/paka/blockchain/basic/Main.java
@@ -3,14 +3,17 @@ package io.paka.blockchain.basic;
 import io.paka.blockchain.basic.block.Block;
 import io.paka.blockchain.basic.block.impl.SimpleBlock;
 import io.paka.blockchain.basic.hash.HashProvider;
+import io.paka.blockchain.basic.hash.impl.Sha256HashProvider;
 import io.paka.blockchain.basic.hash.impl.StabHashProvider;
 
 public class Main {
     public static void main(String[] args) {
 
-        HashProvider provider = new StabHashProvider();
-        Block block = new SimpleBlock("Hello, I'm first block in the chain", null, provider);
+        HashProvider provider = new Sha256HashProvider();
+        Block genesisBlock = new SimpleBlock("Hello, I'm first block in the chain", "Genesis", provider);
+        Block secondBlock = new SimpleBlock("Hello, I'm second block in the chain", genesisBlock.hash(), provider);
 
-        System.out.println(block);
+        System.out.println(genesisBlock);
+        System.out.println(secondBlock);
     }
 }
